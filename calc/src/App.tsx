1b5e4bc1ac8ab1a922aebb898ea38ba284dff345
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-03-23 15:32:24 +0200

    updating styles

--- calc/src/App.tsx
+++ calc/src/App.tsx
@@ -10,19 +10,19 @@ console.log(numbers)
 function App() {
   return (
     <>
-    <div id = "wrapper-full" className = "h-100% border">
+    <div id = "wrapper-full" className = "h-100 border">
       <Header title = "Calculator-react"></Header>
         <div id = "output">
           <p> calculations appear here</p>
         </div>
 
-      <div className = "h-full w-9/10 flex flex-row border m-auto p-3">
+      <div className = "h-[100%] w-9/10 flex flex-row border m-auto p-3">
 
-          <div id = "numblock" className = "w-2/3 h-full m-auto flex flex-grid content-fit">
+          <div id = "numblock" className = "w-2/3 m-auto flex flex-grid content-fit">
             <ButtonBlock content = {numbers} color = "btn-warning"></ButtonBlock>
           </div>
 
-          <div id = "symblock" className = "h-full w-1/4 m-auto flex flex-grid">
+          <div id = "symblock" className = "w-1/4 m-auto flex flex-grid">
             <ButtonBlock content = {symbols} color = "btn-success"></ButtonBlock>
           </div>
 
